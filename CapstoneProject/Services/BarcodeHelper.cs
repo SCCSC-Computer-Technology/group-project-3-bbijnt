83f00aa7c741dac15c02b393b7b573f7665e1a09
Nicholas Chapman-Miller <nicholaschapmanm@gmail.com>
2026-04-21 14:39:07 -0400

    Add System.drawing package, Add barcode helper, add barcode font

---
+++ CapstoneProject/Services/BarcodeHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+
+namespace CapstoneProject.Services
+{
+    public class BarcodeHelper
+    {
+        public static string FormatUUIDForBarcode(string uuid) //formats UUID for code 39 barcode
+        {
+            return "*" + uuid + "*";
+        }
+
+        public static Bitmap CreateBarcodeImage(string barcodeText, string fontFamily = "fre3of9x", int fontSize = 48)
+        {
+            Font font = new Font(fontFamily, fontSize);
+            Bitmap barcodeBitmap = new Bitmap(600, 150);
+            Graphics g = Graphics.FromImage(barcodeBitmap);
+
+            g.Clear(Color.White); //white background for barcode
+            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+
+            Brush textBrush = Brushes.Black;
+            g.DrawString(barcodeText, font, textBrush, new PointF(10, 30));
+            g.Dispose();
+
+            return barcodeBitmap;
+        }
+
+    }
+}
