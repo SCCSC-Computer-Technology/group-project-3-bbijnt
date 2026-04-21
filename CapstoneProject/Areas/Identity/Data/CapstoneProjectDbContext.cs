beca47775899382e5a493e563aadc684dc4c7310
Nicholas Chapman-Miller <nicholaschapmanm@gmail.com>
2026-04-21 11:36:27 -0400

    Update DbContext to seed categories

--- CapstoneProject/Areas/Identity/Data/CapstoneProjectDbContext.cs
+++ CapstoneProject/Areas/Identity/Data/CapstoneProjectDbContext.cs
@@ -36,5 +36,160 @@ public class CapstoneProjectDbContext : IdentityDbContext<CapstoneProjectUser>
             .HasColumnType("decimal(6, 2)");
         modelBuilder.Entity<CapstoneProjectUser>()
             .HasIndex(x=>x.StudentId).IsUnique();
+
+        //SEED DEFAULT DATA FOR CATEGORIES  !!RUN "Update-Database" IN PACKAGE MANAGER CONSOLE TO ADD!!
+        modelBuilder.Entity<ItemCategory>().HasData(
+            new ItemCategory { CategoryID = 1, Name = "Dairy" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 1, CategoryID = 1, Name = "Milk" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 2, CategoryID = 1, Name = "Cheese" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 3, CategoryID = 1, Name = "Yogurt" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 4, CategoryID = 1, Name = "Butter" }
+        );
+
+        modelBuilder.Entity<ItemCategory>().HasData(
+            new ItemCategory { CategoryID = 2, Name = "Produce" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 1, CategoryID = 2, Name = "Fruit" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 2, CategoryID = 2, Name = "Vegetable" }
+        );
+
+
+        modelBuilder.Entity<ItemCategory>().HasData(
+            new ItemCategory { CategoryID = 3, Name = "Canned Products" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 1, CategoryID = 3, Name = "Canned Meat" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 2, CategoryID = 3, Name = "Canned Vegetable" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 3, CategoryID = 3, Name = "Other Can Food" }
+        );
+
+        modelBuilder.Entity<ItemCategory>().HasData(
+            new ItemCategory { CategoryID = 4, Name = "Grains/Pasta" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 1, CategoryID = 4, Name = "Bread" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 2, CategoryID = 4, Name = "Box Pasta" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 3, CategoryID = 4, Name = "Rice" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 4, CategoryID = 4, Name = "Other Grains" }
+        );
+
+        modelBuilder.Entity<ItemCategory>().HasData(
+        new ItemCategory { CategoryID = 5, Name = "Laundry" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 1, CategoryID = 5, Name = "Liquid Detergent" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 2, CategoryID = 5, Name = "Dry Detergent" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 3, CategoryID = 5, Name = "Bleach" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 4, CategoryID = 5, Name = "Other Laundry" }
+        );
+
+        modelBuilder.Entity<ItemCategory>().HasData(
+        new ItemCategory { CategoryID = 6, Name = "Personal Care" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 1, CategoryID = 6, Name = "Body Soap" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 2, CategoryID = 6, Name = "Shampoo" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 3, CategoryID = 6, Name = "Conditioner" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 4, CategoryID = 6, Name = "Toothpaste" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 5, CategoryID = 6, Name = "Hand Soap" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 6, CategoryID = 6, Name = "Deodorant" }
+        );
+
+        modelBuilder.Entity<ItemCategory>().HasData(
+        new ItemCategory { CategoryID = 7, Name = "Baby Care" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 1, CategoryID = 6, Name = "Diapers" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 2, CategoryID = 6, Name = "Wipes" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 3, CategoryID = 6, Name = "Lotion" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 4, CategoryID = 6, Name = "Shampoo" }
+        );
+
+        modelBuilder.Entity<ItemSubcategory>().HasData(
+            new ItemSubcategory { SubcategoryID = 5, CategoryID = 6, Name = "Powder" }
+        );
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
 }
